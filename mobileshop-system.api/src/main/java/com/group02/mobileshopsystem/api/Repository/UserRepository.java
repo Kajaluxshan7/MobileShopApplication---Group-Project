7d9d4bbf3bd9788475eea0a000e553657be66533
Kajaluxshan7 <kajalux-se19005@stu.kln.ac.lk>
2023-04-07 12:58:03 +0530

    removed unwanted declaration

--- mobileshop-system.api/src/main/java/com/group02/mobileshopsystem/api/Repository/UserRepository.java
+++ mobileshop-system.api/src/main/java/com/group02/mobileshopsystem/api/Repository/UserRepository.java
@@ -14,8 +14,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
   Optional<User> findByEmail(String email);
   boolean existsByEmail(String email);
-  Optional<User> findByLink(String verificationToken);
-
   @Transactional
   @Modifying
   @Query("UPDATE User a " +
