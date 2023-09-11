2f95402455786f0c17f203cebc0eed1201edb331
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-11 23:42:19 +0530

    Swagger-Api Documentation

--- backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
+++ backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
@@ -1,9 +1,8 @@
 package com.group02.mobileshopsystem.api;
 
-import org.hibernate.annotations.Bag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,4 +15,3 @@ public class MobileShopApp {
 	}
 
 }
-
