93c0c3e64d58e4096c3020bb88f9ca9ad8d543ef
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-12 13:33:58 +0530

    Removed- Swagger

--- backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
+++ backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @EnableJpaRepositories
 @SpringBootApplication
 public class MobileShopApp {
