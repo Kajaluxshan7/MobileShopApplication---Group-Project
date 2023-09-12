44558d7764d1a3171928f6e4cddcc03662c7c64c
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-12 22:16:39 +0530

    Signup-backend connection impl

--- backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
+++ backend-application/src/main/java/com/group02/mobileshopsystem/api/MobileShopApp.java
@@ -2,9 +2,8 @@ package com.group02.mobileshopsystem.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableJpaRepositories
 @SpringBootApplication
 public class MobileShopApp {
--- frontend-application/src/auth/signup.jsx
+++ frontend-application/src/auth/signup.jsx
@@ -1,34 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import Signin from './signin';
 import './auth.scss'
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+    mobile_no: '',
+    email: '',
+    password: '',
+    user_role: 'user', // Default to 'user' role
+  });
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await axios.post('http://localhost:8080/auth/register', formData);
+      console.log('Registration Successful', response.data);
+
+      // Clear the form after successful registration
+      setFormData({
+        firstname: '',
+        lastname: '',
+        mobile_no: '',
+        email: '',
+        password: '',
+        user_role: 'user',
+      });
+    } catch (error) {
+      console.error('Registration Failed', error);
+    }
+  };
+
   return (
-    <div className='container'>
-        <div className='form-container'>
-            <div className="main">
-                <input type="checkbox" id="check" aria-hidden="true" />
+    <div className="container">
+      <div className="form-container">
+        <div className="main">
+          <input type="checkbox" id="check" aria-hidden="true" />
 
-            <div className="signup">
-                <form>
-                    <label htmlFor="check" aria-hidden="true">
-                        Sign up
-                    </label>
-                    <input type="text" name="fname" placeholder="First Name" required />
-                    <input type="text" name="lname" placeholder="Last Name" required />
-                    <input type="number" name="mobile" placeholder="Mobile No" required />
-                    <input type="email" name="email" placeholder="Email" required />
-                    <input type="password" name="pswd" placeholder="Password" required />
-                    <button type="submit">Sign up</button>
-                </form>
-            </div>
+          <div className="signup">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="check" aria-hidden="true">
+                Sign up
+              </label>
+              
+              <input
+                type="text"
+                name="firstname"
+                placeholder="First Name"
+                value={formData.firstname}
+                onChange={handleInputChange}
+                required
+              />
 
-      <Signin/>
+              <input
+                type="text"
+                name="lastname"
+                placeholder="Last Name"
+                value={formData.lastname}
+                onChange={handleInputChange}
+                required
+              />
+
+              <input
+                type="number"
+                name="mobile_no"
+                placeholder="Mobile No"
+                value={formData.mobile_no}
+                onChange={handleInputChange}
+                required
+              />
+
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
+              
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+                required
+              />
+              {/* Add a select input to choose user or admin */}
+              <select
+                name="user_role"
+                value={formData.user_role}
+                onChange={handleInputChange}
+              >
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+              </select>
+              <button type="submit">Sign up</button>
+            </form>
+          </div>
+
+          <Signin />
+        </div>
       </div>
-
-
-       </div>
-
     </div>
   );
 }
